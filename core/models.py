cf6a1bd5d221848ae993d43f93a7f73c4b89b610
Carlos R. González Jiménez <cranza@gmail.com>
2026-09-26 13:43:36 -0400

    fix: Fixing Customer model in core app

--- core/models.py
+++ core/models.py
@@ -12,9 +12,9 @@ from .generics import PermissionsMeta
 
 
 class Customer(models.Model):
-    bussines_name = models.CharField()
+    bussines_name = models.CharField(max_length=100)
     business_phone = models.CharField(max_length=50, null=True, blank=True)
-    business_email = models.CharField(max_length=100, null=True, blank=True)
+    business_email = models.EmailField(null=True, blank=True)
     nit_code = models.CharField(max_length=100, null=True, blank=True)
     address = models.TextField(null=True, blank=True)
     contacts = models.JSONField(default=list, blank=True)
